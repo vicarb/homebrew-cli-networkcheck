91e87ab51777f6d97e71c7e7040270991d860a6d
vicarb <vcardenasb@protonmail.com>
2023-09-06 23:57:47 -0300

    updated formula urls

--- Formula/cli-network-check.rb
+++ Formula/cli-network-check.rb
@@ -7,12 +7,12 @@ class CliNetworkCheck < Formula
     depends_on "python@3.9"
   
     resource "psutil" do
-      url "https://files.pythonhosted.org/packages/your/path/to/psutil-5.9.5-cp38-abi3-macosx_11_0_arm64.whl"
+      url "https://files.pythonhosted.org/packages/ed/98/2624954f83489ab13fde2b544baa337d5578c07eee304d320d9ba56e1b1f/psutil-5.9.5-cp38-abi3-macosx_11_0_arm64.whl"
       sha256 "c607bb3b57dc779d55e1554846352b4e358c10fff3abf3514a7a6601beebdb30"
     end
   
     resource "termcolor" do
-      url "https://files.pythonhosted.org/packages/your/path/to/termcolor-2.3.0.tar.gz"
+      url "https://files.pythonhosted.org/packages/b8/85/147a0529b4e80b6b9d021ca8db3a820fcac53ec7374b87073d004aaf444c/termcolor-2.3.0.tar.gz"
       sha256 "b5b08f68937f138fe92f6c089b99f1e2da0ae56c52b78bf7075fd95420fd9a5a"
     end
   
